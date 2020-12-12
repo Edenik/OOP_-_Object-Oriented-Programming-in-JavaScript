b3edb16459a9cd1fe6491e09d00d379c917cf77b
Edenik <Edenik5@gmail.com>
2020-12-13 01:43:51 +0200

    StopWatch Exercise

---
+++ 1 - Objects/index.js
@@ -0,0 +1,34 @@
+function StopWatch() {
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function () {
+        if (running)
+            throw new Error('Stopwatch has already started.');
+
+        running = true;
+        startTime = new Date();
+    }
+
+    this.stop = function () {
+        if (!running)
+        throw new Error('Stopwatch is not started.');
+
+        running = false;
+        endTime = new Date();
+        duration += (endTime.getTime() - startTime.getTime()) / 1000;
+    }
+
+    this.reset = function () {
+        duration = 0;
+        startTime, endTime = null;
+        running = false;
+    }
+
+    Object.defineProperty(this, 'duration', {
+        get: function () {
+            return duration;
+        }
+    })
+}
+
+const sw = new StopWatch()
