fb871f2098a99c4f8592350434ef09cb8670d8e1
Edenik <Edenik5@gmail.com>
2020-12-15 12:31:57 +0200

    ES6 Classes

---
+++ 4 - ES6 Classes/index.js
@@ -0,0 +1,42 @@
+// Stack.push() 
+//stack.count 
+// stack.pop()
+// stack.peek()
+
+const _items = new WeakMap();
+
+class Stack {
+    constructor() {
+        _items.set(this, []);
+    }
+
+    get count() {
+        return _items.get(this.length);
+    }
+
+    push(item) {
+        _items.get(this).push(item);
+    }
+
+    pop() {
+        const items = _items.get(this)
+
+        if (items.length === 0)
+            throw new Error('Stack is empty');
+        return items.pop()
+    }
+
+    peek() {
+        const items = _items.get(this)
+
+        if (items.length === 0)
+            throw new Error('Stack is empty');
+        return items[items.length - 1]
+    }
+
+    get count(){
+        return _items.get(this).length;
+    }
+}
+
+const stack = new Stack();
